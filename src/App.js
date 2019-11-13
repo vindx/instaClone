a797479b8fa323719c0c23276baed22fadfce091
vindx <kiruha9414@gmail.com>
2019-11-13 21:47:05 +0300

    created AdminPage

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import {BrowserRouter, Route} from "react-router-dom";
 import Header from "./components/Header/Header";
 import Posts from "./components/Posts/Posts";
 import Profile from "./components/Profile/Profile";
+import AdminPage from "./components/AdminPage/AdminPage";
 
 function App() {
     return (
         <BrowserRouter>
             <div className="App">
+                <Route exact path='/admin' component={AdminPage}/>
+
                 <Route exact path='/posts' component={Header}/>
-                <Route exact path='/posts' component={Posts}/>
+                <Route exact path='/posts' render={() => <Posts/>}/>
 
                 <Route exact path='/profile' component={Header}/>
-                <Route exact path='/profile' component={Profile}/>
+                <Route exact path='/profile' render={() => <Profile/>}/>
 
                 <Route exact path='/' component={SignUp}/>
                 <Route exact path='/login' component={LogIn}/>
---
+++ src/components/AdminPage/AdminPage.js
@@ -0,0 +1,129 @@
+import React from "react";
+import {makeStyles} from "@material-ui/core/styles";
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import styles from "./AdminPage.module.css";
+import {Link} from "react-router-dom";
+
+const columns = [
+    {id: 'userName', label: 'Username'},
+    {id: 'email', label: 'Email'},
+    {id: 'fullName', label: 'Full name', align: 'center'},
+    {id: 'removeRequest', label: 'Remove request', align: 'center'},
+];
+
+const dataBase = [{userName: "sasdqwgg", email: "3tetwe@opwla.csa", fullName: "Acdb OPs", removeRequest: true},
+    {userName: "k_karelin", email: "ksisk@gmail.com", fullName: "Kiryl Karelin", removeRequest: false},
+    {userName: "awswa_sad", email: "sdsdsds@dasdas.caa", fullName: "", removeRequest: false},
+    {userName: "sasdqwgg", email: "3tetwe@opwla.csa", fullName: "Acdb OPs", removeRequest: true},
+    {userName: "k_karelin", email: "ksisk@gmail.com", fullName: "Kiryl Karelin", removeRequest: false},
+    {userName: "awswa_sad", email: "sdsdsds@dasdas.caa", fullName: "", removeRequest: false},
+    {userName: "sasdqwgg", email: "3tetwe@opwla.csa", fullName: "Acdb OPs", removeRequest: true},
+    {userName: "k_karelin", email: "ksisk@gmail.com", fullName: "Kiryl Karelin", removeRequest: false},
+    {userName: "awswa_sad", email: "sdsdsds@dasdas.caa", fullName: "", removeRequest: false},
+    {userName: "sasdqwgg", email: "3tetwe@opwla.csa", fullName: "Acdb OPs", removeRequest: false},
+    {userName: "k_karelin", email: "ksisk@gmail.com", fullName: "Kiryl Karelin", removeRequest: false},
+    {userName: "awswa_sad", email: "sdsdsds@dasdas.caa", fullName: "", removeRequest: false},
+    {userName: "sasdqwgg", email: "3tetwe@opwla.csa", fullName: "Acdb OPs", removeRequest: true},
+];
+
+const useStyles = makeStyles({
+    root: {
+        width: '80%',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: 50,
+    },
+    tableWrapper: {
+        height: 595,
+        overflow: 'auto',
+    },
+    tableHead: {
+        fontWeight: 600,
+        color: 'rgba(0, 0, 0, 1)',
+    },
+});
+
+const AdminPage = () => {
+    const classes = useStyles();
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = event => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
+
+    return (
+        <>
+            <Paper className={classes.root}>
+                <div className={classes.tableWrapper}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                {columns.map(column => (
+                                    <TableCell
+                                        key={column.id}
+                                        align={column.align}
+                                        className={classes.tableHead}
+                                    >
+                                        {column.label}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {dataBase.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => {
+                                return (
+                                    <TableRow tabIndex={-1} key={row.code}>
+                                        {columns.map(column => {
+                                            const value = row[column.id];
+                                            return (
+                                                <TableCell key={column.id} align={column.align} style={{height: 25}}>
+                                                    {typeof value === 'boolean' && value ?
+                                                        <button className={styles.button}>Delete profile</button>
+                                                        : value}
+                                                </TableCell>
+                                            );
+                                        })}
+                                    </TableRow>
+                                );
+                            })}
+                        </TableBody>
+                    </Table>
+                </div>
+                <TablePagination
+                    rowsPerPageOptions={[10, 25, 50]}
+                    component="div"
+                    count={dataBase.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    backIconButtonProps={{
+                        'aria-label': 'previous page',
+                    }}
+                    nextIconButtonProps={{
+                        'aria-label': 'next page',
+                    }}
+                    onChangePage={handleChangePage}
+                    onChangeRowsPerPage={handleChangeRowsPerPage}
+                />
+            </Paper>
+            <div className={styles.logOutButtonContainer}>
+                <Link to='/'>
+                    <button className={styles.button}>Log Out</button>
+                </Link>
+            </div>
+        </>
+    )
+};
+
+export default AdminPage;
