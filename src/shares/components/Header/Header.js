c0f9dcaf65497171d8144b46e6b9d358ab72c309
vindx <kiruha9414@gmail.com>
2019-12-25 01:05:05 +0300

    some minor changes with Header component

--- src/shares/components/Header/Header.js
+++ src/shares/components/Header/Header.js
@@ -1,50 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Grid, TextField } from '@material-ui/core';
-import PropTypes from 'proptypes';
 
 import styles from './Header.module.scss';
+import LogOutContainer from '../LogOut/LogOutContainer';
 
-const Header = props => {
-  const { postsUrl } = props;
-
-  return (
-    <Container className={styles.container} maxWidth="xl">
-      <Grid container direction="row" justify="space-evenly" alignItems="center">
-        <Grid item xs={2} className={styles.logo}>
-          <Link to={postsUrl} onClick={() => window.scrollTo(0, 0)}>
-            <Grid container direction="row" wrap="nowrap" justify="flex-start" alignItems="center">
-              <img
-                alt=""
-                src="https://www.instagram.com/static/images/ico/favicon.svg/fc72dd4bfde8.svg"
-              />
-              <div className={styles.separator}>|</div>
-              <div className={styles.headerText}>InstaClone</div>
-            </Grid>
-          </Link>
-        </Grid>
-        <Grid item xs={4}>
-          <TextField
-            className={styles.textField}
-            variant="outlined"
-            id="search"
-            label="Search"
-            type="search"
-            margin="dense"
-          />
-        </Grid>
-        <Grid item className={styles.profileLogo}>
-          <Link to="/profile" onClick={() => window.scrollTo(0, 0)}>
-            <span className="material-icons">perm_identity</span>
-          </Link>
-        </Grid>
+const Header = () => (
+  <Container className={styles.container} maxWidth="xl">
+    <Grid container direction="row" justify="space-evenly" alignItems="center">
+      <Grid item xs={2} className={styles.logo}>
+        <Link to="/" onClick={() => window.scrollTo(0, 0)}>
+          <Grid container direction="row" wrap="nowrap" justify="flex-start" alignItems="center">
+            <img
+              alt=""
+              src="https://www.instagram.com/static/images/ico/favicon.svg/fc72dd4bfde8.svg"
+            />
+            <div className={styles.separator}>|</div>
+            <div className={styles.headerText}>InstaClone</div>
+          </Grid>
+        </Link>
       </Grid>
-    </Container>
-  );
-};
-
-Header.propTypes = {
-  postsUrl: PropTypes.string.isRequired,
-};
+      <Grid item xs={4}>
+        <LogOutContainer />
+        {/*<TextField*/}
+        {/*  className={styles.textField}*/}
+        {/*  variant="outlined"*/}
+        {/*  id="search"*/}
+        {/*  label="Search"*/}
+        {/*  type="search"*/}
+        {/*  margin="dense"*/}
+        {/*/>*/}
+      </Grid>
+      <Grid item className={styles.profileLogo}>
+        <Link to="/profile" onClick={() => window.scrollTo(0, 0)}>
+          <span className="material-icons">perm_identity</span>
+        </Link>
+      </Grid>
+    </Grid>
+  </Container>
+);
 
 export default Header;
