d39a8cc00f81e165b5582883659e9770d050abdd
vindx <kiruha9414@gmail.com>
2020-01-12 17:48:27 +0300

    changed user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,24 +1,13 @@
 const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  fullName: {
-    type: String,
-    default: '',
-  },
-  userName: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+  email: { type: String, required: true, unique: true },
+  fullName: { type: String, default: '' },
+  userName: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  posts: { type: Array, default: [] },
+  profilePhoto: { type: String, default: '' },
+  removeRequest: { type: Boolean, default: false },
 });
 
 module.exports = model('User', userSchema);
