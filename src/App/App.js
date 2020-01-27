b127b401490fcb9ef2a2a519e3a2465c19b60a21
vindx <kiruha9414@gmail.com>
2020-01-27 18:50:50 +0300

    rebuilt app component from class to functional component

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
@@ -11,26 +11,24 @@ import SignUpOrLogInPage from '../Pages/SignUpOrLogInPage/SignUpOrLogInPage';
 import PageNotFound from '../shares/components/PageNotFound/PageNotFound';
 import { initialize } from '../redux/reducers/appReducer';
 
-class App extends React.Component {
-  componentDidMount() {
-    this.props.initialize();
-  }
+const App = props => {
+  useEffect(() => {
+    props.initialize();
+  }, [props.initialize]);
 
-  render() {
-    if (!this.props.initialized) return <BigPreloader />;
-    return (
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={PostsPage} />
-          <Route path="/accounts/:action" component={SignUpOrLogInPage} />
-          <Route exact path="/admin" component={AdminPageContainer} />
-          <Route path="/profile/:userName?" component={ProfilePage} />
-          <PageNotFound />
-        </Switch>
-      </div>
-    );
-  }
-}
+  if (!props.initialized) return <BigPreloader />;
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={PostsPage} />
+        <Route path="/accounts/:action" component={SignUpOrLogInPage} />
+        <Route exact path="/admin" component={AdminPageContainer} />
+        <Route path="/profile/:userName?" component={ProfilePage} />
+        <PageNotFound />
+      </Switch>
+    </div>
+  );
+};
 
 const mapStateToProps = state => ({
   initialized: state.app.initialized,
