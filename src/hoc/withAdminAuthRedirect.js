674fb8a9b617f64e2e198e4c34386d81cd68a7b4
vindx <kiruha9414@gmail.com>
2020-01-27 17:52:35 +0300

    some changes on HOC (if admin -> redirect)

--- src/hoc/withAdminAuthRedirect.js
+++ src/hoc/withAdminAuthRedirect.js
@@ -3,12 +3,12 @@ import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 
 const mapStateToProps = state => ({
-  activeUser: state.auth.activeUser,
+  activeUser: state.auth.data,
 });
 
 const withAdminAuthRedirect = Component => {
   const AuthRedirectedComponent = props => {
-    if (props.activeUser === 'admin') return <Redirect to="/admin" />;
+    if (props.activeUser && props.activeUser.role === 'admin') return <Redirect to="/admin" />;
     return <Component {...props} />;
   };
   return connect(mapStateToProps)(AuthRedirectedComponent);
