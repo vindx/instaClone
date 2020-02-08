a511a56a998b0013336a64bc99bd86dcef30b891
vindx <kiruha9414@gmail.com>
2020-02-08 16:53:55 +0300

    minor update Header component

--- src/shares/components/Header/Header.js
+++ src/shares/components/Header/Header.js
@@ -1,41 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { Container, Grid } from '@material-ui/core';
 
+import { getAllPosts } from '../../../redux/actions/postsActions';
 import styles from './Header.module.scss';
 
-const Header = () => (
-  <Container className={styles.container} maxWidth="xl">
-    <Grid container direction="row" justify="space-evenly" alignItems="center">
-      <Grid item xs={2} className={styles.logo}>
-        <Link to="/" onClick={() => window.scrollTo(0, 0)}>
-          <Grid container direction="row" wrap="nowrap" justify="flex-start" alignItems="center">
-            <img
-              alt=""
-              src="https://www.instagram.com/static/images/ico/favicon.svg/fc72dd4bfde8.svg"
-            />
-            <div className={styles.separator}>|</div>
-            <div className={styles.headerText}>InstaClone</div>
-          </Grid>
-        </Link>
-      </Grid>
-      <Grid item xs={4}>
-        {/*<TextField*/}
-        {/*  className={styles.textField}*/}
-        {/*  variant="outlined"*/}
-        {/*  id="search"*/}
-        {/*  label="Search"*/}
-        {/*  type="search"*/}
-        {/*  margin="dense"*/}
-        {/*/>*/}
-      </Grid>
-      <Grid item className={styles.profileLogo}>
-        <Link to="/profile" onClick={() => window.scrollTo(0, 0)}>
-          <span className="material-icons">perm_identity</span>
-        </Link>
-      </Grid>
-    </Grid>
-  </Container>
-);
+const Header = props => {
+  const getPosts = () => {
+    props.getAllPosts(props.userData.userId);
+    window.scrollTo(0, 0);
+  };
 
-export default Header;
+  return (
+    <Container className={styles.container} maxWidth="xl">
+      <Grid container direction="row" justify="space-evenly" alignItems="center">
+        <Grid item xs={2} className={styles.logo}>
+          <Link to="/" onClick={getPosts}>
+            <Grid container direction="row" wrap="nowrap" justify="flex-start" alignItems="center">
+              <img
+                alt=""
+                src="https://www.instagram.com/static/images/ico/favicon.svg/fc72dd4bfde8.svg"
+              />
+              <div className={styles.separator}>|</div>
+              <div className={styles.headerText}>InstaClone</div>
+            </Grid>
+          </Link>
+        </Grid>
+        <Grid item xs={4}>
+          {/*<TextField*/}
+          {/*  className={styles.textField}*/}
+          {/*  variant="outlined"*/}
+          {/*  id="search"*/}
+          {/*  label="Search"*/}
+          {/*  type="search"*/}
+          {/*  margin="dense"*/}
+          {/*/>*/}
+        </Grid>
+        <Grid item className={styles.profileLogo}>
+          <Link to="/profile" onClick={() => window.scrollTo(0, 0)}>
+            <span className="material-icons">perm_identity</span>
+          </Link>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+const mapStateToProps = state => ({
+  userData: state.auth.data,
+});
+
+export default connect(mapStateToProps, { getAllPosts })(Header);
