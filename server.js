be7b8d53de63ea35c56054759fa53c65b53c4b72
vindx <kiruha9414@gmail.com>
2020-02-17 16:54:31 +0300

    preparations for deploying

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const config = require('config');
+const path = require('path');
 const mongoose = require('mongoose');
 
-const server = express();
+const app = express();
 
-server.use(express.json({ extended: true }));
+app.use(express.json({ extended: true }));
 
-server.use('/api/auth', require('./routes/auth.routes'));
-server.use('/api/users', require('./routes/users.routes'));
-server.use('/api/posts', require('./routes/posts.routes'));
-server.use('/api/tags', require('./routes/tags.routes'));
+app.use('/api/auth', require('./routes/auth.routes'));
+app.use('/api/users', require('./routes/users.routes'));
+app.use('/api/posts', require('./routes/posts.routes'));
+app.use('/api/tags', require('./routes/tags.routes'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 const PORT = config.get('port') || 5000;
 
@@ -21,7 +30,7 @@ const start = async () => {
       useCreateIndex: true,
       useFindAndModify: false,
     });
-    server.listen(PORT, () => console.log(`App has been started at port ${PORT}...`));
+    app.listen(PORT, () => console.log(`App has been started at port ${PORT}...`));
   } catch (e) {
     console.log('Server ERROR!', e.message);
     process.exit(1);
